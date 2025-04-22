d03b572abefe23ee4cd0d9d0624fbb6c79038156
Kunal Raj Pandey <pandeykunalraj@gmail.com>
2025-04-23 00:07:34 +0530

    second commit
    Database Connection

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./connection.js";
 
 dotenv.config();
 
 const app = express();
 
 const PORT =process.env.PORT || 5000;
+
+connectDB();
 
 app.listen(PORT,()=>{
     console.log(`Server Running on port ${PORT}`);
