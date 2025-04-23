24872c5cde65a61152858eb352516f3d04e1c083
Kunal Raj Pandey <pandeykunalraj@gmail.com>
2025-04-24 00:08:42 +0530

    Fourth commit
    basic middlewares setup

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./connection.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
-
 const app = express();
 
-const PORT =process.env.PORT || 5000;
+
+app.use(cors({
+    origin : [process.env.FRONTEND_URL],
+    methods :["GET","POST","PUT","DELETE"],
+    credentials : true,
+})
+);
+
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
