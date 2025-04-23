bfbb01c0a39c9baeb4beedccd168766a659c1fbd
Kunal Raj Pandey <pandeykunalraj@gmail.com>
2025-04-23 23:20:29 +0530

    Third Commit
    Frontend + Tailwind Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-red-500 hover:text-blue-500 hover:underline text-center my-20 font-bold '>
+      LIBRARY MANAGEMENT SYSTEM
+    </div>
+  )
+}
+
+export default App
