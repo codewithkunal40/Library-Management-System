3a8a0270ce74c0da96e80d6aaec6fc386f3db1ec
Kunal Raj Pandey <pandeykunalraj@gmail.com>
2025-04-24 14:21:49 +0530

    fifth commit
    Middlewares setup

--- server/server.js
+++ server/server.js
@@ -3,27 +3,28 @@ import dotenv from "dotenv";
 import connectDB from "./connection.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { errorMiddleware } from "./middlewares/errorMiddlewares.js";
 
 dotenv.config();
 const app = express();
 
-
-app.use(cors({
-    origin : [process.env.FRONTEND_URL],
-    methods :["GET","POST","PUT","DELETE"],
-    credentials : true,
-})
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
 );
-
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 5000;
 
-connectDB();
+app.listen(PORT, () => {
+  console.log(`Server Running on port ${PORT}`);
+});
 
-app.listen(PORT,()=>{
-    console.log(`Server Running on port ${PORT}`);
-})
+connectDB();
+app.use(errorMiddleware);
