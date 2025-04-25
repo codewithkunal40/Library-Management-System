031da3fe43dfd94d02b1a3f9cf0d7d8fc3956812
Syed Faraz Ahmad <farazahmadsfa@gmail.com>
2025-04-25 21:18:35 +0530

    ..

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,29 @@
+// import React from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import Login from "./components/Login";
+// import Register from "./components/Register";
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <div className="App">
+//         <ToastContainer />
+//         <Switch>
+//           <Route path="/login" component={Login} />
+//           <Route path="/register" component={Register} />
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
+
+
+
 import React from 'react'
 
 const App = () => {
