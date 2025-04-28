1095deec179653a67d15f58dca47ea5376e3765c
Kunal Raj Pandey <pandeykunalraj@gmail.com>
2025-04-28 13:28:38 +0530

    update navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,35 +4,35 @@ const Navbar = ({ setShowPopup }) => {
   return (
     <nav className="bg-blue-900 shadow-md p-4 flex justify-between items-center">
       <div className="flex space-x-4">
-        <Link to="/" className="text-red-500 hover:text-red-600 font-medium">
+        <Link to="/" className="text-yellow-500 hover:text-red-600 font-medium">
           Home
         </Link>
         <Link
           to="/about"
-          className="text-red-500 hover:text-red-600 font-medium"
+          className="text-yellow-500 hover:text-red-600 font-medium"
         >
           About
         </Link>
         <Link
           to="/contact"
-          className="text-red-500 hover:text-red-600 font-medium"
+          className="text-yellow-500 hover:text-red-600 font-medium"
         >
           Contact
         </Link>
         <Link
           to="/login"
-          className="text-red-500 hover:text-red-600 font-medium"
+          className="text-yellow-500 hover:text-red-600 font-medium"
         >
           Login
         </Link>
         <button
           onClick={() => setShowPopup(true)}
-          className="text-red-500 hover:text-red-600 font-medium"
+          className="text-yellow-500 hover:text-red-600 font-medium"
         >
           Sign Up
         </button>
       </div>
-      <div className="text-2xl font-bold text-red-500">
+      <div className="text-2xl font-bold text-yellow-500 hover:text-red-600">
         Library Management System
       </div>
     </nav>
