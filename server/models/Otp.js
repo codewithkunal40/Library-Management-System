cffd9c8feca2d9aa6a80f91318858a81a5baf3ff
Syed Faraz Ahmad <farazahmadsfa@gmail.com>
2025-04-30 00:10:35 +0530

    ..

--- server/models/Otp.js
+++ server/models/Otp.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const otpSchema = new mongoose.Schema({
   email: String,
   otp: String,
-  createdAt: { type: Date, default: Date.now, expires: 300 }, // 5 mins
+  createdAt: { type: Date, default: Date.now, expires: 600 }, // 5 mins
 });
 
 const Otp = mongoose.model("Otp", otpSchema);
