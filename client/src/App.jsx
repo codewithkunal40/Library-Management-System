913c30ca2378fd63b2dcad9db4406b9f11a896bb
Syed Faraz Ahmad <farazahmadsfa@gmail.com>
2025-04-30 00:40:20 +0530

    welcome page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
+import WelcomePage from './components/welcomePage'
 
 const App = () => {
   return (
-    <div className='text-center font-bold text-red-500 hover:text-red-900 my-18 hover:underline hover:font-semibold'>
-      Library Management System
-    </div>
+    <WelcomePage />
   )
 }
 
