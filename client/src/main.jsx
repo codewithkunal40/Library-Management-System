54eda0a25f4da69b6965a89b365abab162daade5
Syed Faraz Ahmad <farazahmadsfa@gmail.com>
2025-05-06 14:08:46 +0530

    toastify

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -17,7 +17,7 @@ createRoot(document.getElementById('root')).render(
       closeOnClick
       pauseOnHover
       draggable
-      theme="light"
+      theme="colored"
     />
   </StrictMode>,
 )
