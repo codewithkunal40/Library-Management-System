5c6387c21abdeacc9051e83f333503a0524cd15a
Syed Faraz Ahmad <farazahmadsfa@gmail.com>
2025-05-06 14:09:54 +0530

    toast

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,7 +9,6 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <ToastContainer
-      style={{ zIndex: 9999 }}
       position="top-right"
       autoClose={3000}
       hideProgressBar={false}
